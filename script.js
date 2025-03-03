21c77e4f36bd559e7bff4a70b2f58dd72a07eeff
Shanelle Cassandra Buendia <scdbuendia22@src.pshs.edu.ph>
2025-03-03 19:21:28 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function hiddenMessage(){
+    let message = prompt('Enter a Message');
+    let result = "";
+
+    if (message.includes("FR")) {
+        result = "The message \" " + message + "\" is legitimate!";
+    } else if (message.includes("FR") && message.includes("AI")) {
+        result = "The message \" " + message + "\" is legitimate!";
+    } else if (message.includes("AI")) {
+        result = "The message \" " + message + "\" is tampered with!";
+    } else if (message.includes("aI") || message.includes("Ai")) {
+        result = "The message \" " + message + "\" is tampered with!";
+    } else {
+        result = "The message \" " + message + "\" is yet to be encoded.";
+    }
+
+    document.getElementById("result").innerText = result;
+}
